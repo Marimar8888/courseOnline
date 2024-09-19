48990bdbff6b74aa526970990d91ba7ec79d7029
Marimar <malonso@quadram.mobi>
2024-09-19 18:56:53 +0200

    modularización courseItem

--- src/components/course/course-container.js
+++ src/components/course/course-container.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faWindowRestore } from '@fortawesome/free-solid-svg-icons';
-import axios from 'axios';
-import { API_URL } from '../utils/constant';
+
+import CourseItem from './course-item';
 
 //PEDIENTE DE HACER SCROLL INFINITO
 class CourseContainer extends Component {
@@ -20,10 +18,9 @@ class CourseContainer extends Component {
             limit: 10,
             courseModalIsOpen: false
         }
-        this.onScroll = this.onScroll.bind(this);
-        window.addEventListener("scroll", this.onScroll, false);
+
         this.handleDeleteClick = this.handleDeleteClick.bind(this);
-        this.handleNewCourseClick = this.handleNewCourseClick.bind(this);    
+        this.handleNewCourseClick = this.handleNewCourseClick.bind(this);
     }
 
     handleNewCourseClick() {
@@ -37,46 +34,6 @@ class CourseContainer extends Component {
         console.log("delete course click");
     }
 
-    onScroll() {
-           
-        if (
-            this.state.isLoading ||
-            this.state.courses.length === this.state.totalCount
-          ) {
-            return;
-          }
-            if (window.innerHeight + window.scrollY >= document.documentElement.scrollHeight - 100) {
-                console.log("get more");
-            }
-            if (this.state.param) { 
-                this.getCoursesByType(1, this.state.param);  // Corrección: usa 1 directamente como argumento
-            } else {
-                this.getCoursesByType(1, this.state.param);  // De igual manera aquí
-            }
-    }
-
-    getCoursesByType = (professorId, TypeId) => {
-        const token = localStorage.getItem("token");
-        if (token) {
-          axios
-            .get(
-              `${API_URL}/courses/${professorId}/type/${TypeId}`,
-              {
-                headers: {
-                  Authorization: `Bearer ${token}`,
-                },
-              }
-            )
-            .then((response) => {
-              console.log("getCoursesByType response: ", response);
-              this.setState({ courses: response.data.courses }); // Actualiza el estado con los cursos
-            })
-            .catch((error) => {
-              console.log("getCoursesByType error: ", error);
-            });
-        }
-      };
-
     render() {
         const { courses } = this.state;
         console.log("Courses-container", courses);
@@ -88,7 +45,12 @@ class CourseContainer extends Component {
         }
         return (
             <div>
-                <div className="course-content-page-wrapper">
+                <CourseItem
+                    courses={courses}
+                    handleDeleteClick = {this.handleDeleteClick}
+                    handleNewCourseClick = {this.handleNewCourseClick}
+                />
+                {/* <div className="course-content-page-wrapper">
                     {courses.length > 0 ? (
                         <ul>
                             {courses.map(course => {
@@ -123,7 +85,7 @@ class CourseContainer extends Component {
                             <FontAwesomeIcon icon="plus-circle" />
                         </a>
                     </div>
-                </div>
+                </div> */}
 
             </div >
         );
