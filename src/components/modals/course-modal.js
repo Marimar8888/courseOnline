3c800e920c5638cc539f0835bb95f1d24c05a3ee
Marimar <malonso@quadram.mobi>
2024-09-23 11:06:31 +0200

    estilos al modal

--- src/components/modals/course-modal.js
+++ src/components/modals/course-modal.js
@@ -3,12 +3,26 @@ import ReactModal from "react-modal";
 
 export default class CourseModal extends Component {
     constructor(props) {
-        super(props);        
+        super(props);
+        this.customStyles = {
+            content: {
+                top: "50%",
+                left: "50%",
+                right: "auto",
+                marginRight: "-50%",
+                transform: "translate(-50%,-50%)",
+                width: "800px"
+            },
+            overlay: {
+                backgroundColor: "rgba(1,1,1,0.75)"
+            }
+        }
     }
-    
-    render() { 
+
+    render() {
         return (
-            <ReactModal 
+            <ReactModal
+                style={this.customStyles}
                 isOpen={this.props.modalIsOpen}
                 onRequestClose={() => {
                     this.props.handleModalClose();
