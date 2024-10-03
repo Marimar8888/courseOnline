6b2c094bb533281735dffc73fa7521086c9bdb9e
Marimar <malonso@quadram.mobi>
2024-10-03 10:00:23 +0200

    modificación texto aceptación

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -80,7 +80,7 @@ const Contact = () => {
               onChange={handleChange}
             />
             <label className="acceptConditions" htmlFor="acceptConditions">
-              Autorizo. De conformidad con lo establecido en el Reglamento Europeo de Protección de Datos (UE 2016/679), usted queda informado y presta su consentimiento expreso e inequívoco a la incorporación de los datos que nos facilita en los ficheros de datos personales responsabilidad de TECNOLOGÍA CONTABLE APLICADA, S.L. (B95198321) con domicilio social en, Hurtado de Amézaga, 27. Pl. 10ª, 48008 BILBAO - Bizkaia - Tel.: +34 911 17 61 00, donde podrá ejercitar sus derechos de acceso, rectificación, cancelación y oposición mediante una carta dirigida a esta dirección o a través de email a info@comeralia.com.
+              Autorizo. De conformidad con lo establecido en el Reglamento Europeo de Protección de Datos (UE 2016/679), usted queda informado y presta su consentimiento expreso e inequívoco a la incorporación de los datos que nos facilita en los ficheros de datos personales responsabilidad de FORMACION ONLINE, S.L. (BBB232323) con domicilio social en, Calle Mayor, s/n, 20000 DONOSTIA - Gipuzkoa - Tel.: +34 , donde podrá ejercitar sus derechos de acceso, rectificación, cancelación y oposición mediante una carta dirigida a esta dirección o a través de email a info@cursoonline.com.
             </label>
           </div>
           <button 
