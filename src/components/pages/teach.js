c970dd78d0e5f553d9600f3d5d66198200640c94
Marimar <malonso@quadram.mobi>
2024-10-05 13:50:29 +0200

    estructura y stilos de teach

--- src/components/pages/teach.js
+++ src/components/pages/teach.js
@@ -1,65 +1,62 @@
 import React from 'react';
+import woman from "./../../../static/assets/images/home/portrait-smiling-casual-woman.jpg";
+import happy from "./../../../static/assets/images/home/happy.jpg";
+import happyTogether from "./../../../static/assets/images/home/community-young-people-happy-together.jpg";
+import people from "./../../../static/assets/images/home/people-collage-design.jpg";
+
 
 export default function () {
   return (
     <div className="content-page-wrapper">
       <div className='content-page-header'>
-        <h1>Ven a enseñar con nosotros</h1>
-        <h3>Conviértete en instructor y cambia vidas, incluida la tuya</h3>
+      <h1 className="header-title">Ven a formar a estudiantes</h1>
+      <h3 className="header-subtitle">Cambia tu vida y la de miles de personas.</h3>
+      <button className="btn">Empieza ya</button>
+        <img className="header-image" src={woman} alt="header home image" />
       </div>
       <div className='content-page-content-1'>
         <h1 className='content-1-title'>Hay tantas razones para empezar</h1>
         <div className='content-page-content-1-resons'>
           <div className='content-1-reason'>
-            <h3>Vive de lo que más te gusta</h3>
+            <h2>Vive de lo que más te gusta</h2>
+            <img className="reason-image-1" src={happy} alt="header home image" />
           </div>
           <div className='content-1-reason'>
-            <h3>Desarrollate como Instructor</h3>
+            <h2>Desarrollate como formador</h2>
+            <img className="reason-image-2" src={people} alt="header home image" />            
           </div>
           <div className='content-1-reason'>
-            <h3>Cambia vidas</h3>
+            <h2>Cambia vidas</h2>
+            <img className="reason-image-3" src={happyTogether} alt="header home image" />
           </div>
         </div>
       </div>
       <div className='content-page-content-2'>
         <div className="content-2-text">
-          <h1>12M</h1>
+          <h1>+ 12M</h1>
           <span>Empresas</span>
         </div>
         <div className="content-2-text">
-          <h1>20M</h1>
+          <h1>+ 20M</h1>
           <span>Alumnos</span>
         </div>
         <div className="content-2-text">
-          <h1>1M</h1>
+          <h1>+ 1M</h1>
           <span>Profesores</span>
         </div>
         <div className="content-2-text">
-          <h1>10</h1>
+          <h1>+ 10</h1>
           <span>Paises</span>
         </div>
         <div className="content-2-text">
-          <h1>8</h1>
+          <h1>En 8</h1>
           <span>Idiomas</span>
         </div>
       </div>
       <div className='content-page-content-3'>
-        <h1>Cómo empezar</h1>
-        <span>
-          El primer paso lo das con tu pasión y conocimiento. Después, solo tienes que escoger un tema prometedor con la ayuda de nuestra herramienta Marketplace Insights.
-          Tú decides la forma de enseñar, cómo le vas a poner tu sello propio.
-          ¿Cómo podemos ayudarte?
-          Ofrecemos numerosos recursos con información para crear tu primer curso. Y nuestro panel del instructor y las páginas del programa de curso te ayudarán a mantenerlo todo organizado.
-        </span>
-      </div>
-      <div className='content-page-content-4'>
-        <h1>No tendrás que hacerlo a solas</h1>
-        <h3>Nuestro equipo de asistencia al instructor está aquí para responder a tus preguntas y revisar tu vídeo de prueba, mientras que nuestro Teaching Center te ofrece numerosos recursos para ayudarte a lo largo del proceso. Además, recibe la ayuda de instructores con experiencia en nuestras comunidades en línea.</h3>
-      </div>
-      <div className='content-page-content-5'>
-        <h1>Conviértete en instructor ahora</h1>
-        <span>Únete a una de las tiendas virtuales de aprendizaje en línea más grandes del mundo.</span>
-        <button>Empezar</button>
+        <h1>Conviértete en formador ahora</h1>
+        <span>Únete a nosotros mediante la tienda virtual de aprendizaje en línea.</span>
+        <button className="btn">Empezar</button>
       </div>
     </div>
   )
