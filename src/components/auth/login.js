9b4cb06f18538609c0729befec5419d34f3345a8
Marimar <malonso@quadram.mobi>
2024-10-09 14:29:52 +0200

    modularización del login form

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
 import axios from 'axios';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import { API_URL } from '../utils/constant';
 import EmailRecovery from './email-recovery';
+import LoginFormFields from '../forms/login-form-fields';
 
 export default class Login extends Component {
   constructor(props) {
@@ -110,8 +111,6 @@ export default class Login extends Component {
           this.props.handleUnsuccessfulAuth();
         }
       });
-
-
   }
 
   render() {
@@ -119,43 +118,14 @@ export default class Login extends Component {
       return <EmailRecovery toggleEmailRecovery={this.toggleEmailRecovery}/>
     }
     return (
-      <div className="login-modal-wrapper">
-        <div className='title-login'>
-          <h2>INICIA SESIÓN EN STUDY ONLINE</h2>
-        </div>
-        <form onSubmit={this.handleSubmit} className="auth-form-wrapper">
-          <div className="form-group">
-            <FontAwesomeIcon icon="envelope" />
-            <input
-              type="email"
-              name="email"
-              placeholder="Your email"
-              value={this.state.email}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div className="form-group">
-            <FontAwesomeIcon icon="lock" />
-            <input
-              type="password"
-              name="password"
-              placeholder="Your password"
-              value={this.state.password}
-              onChange={this.handleChange}
-            />
-          </div>
-
-          <div className="errorText">{this.state.errorText}</div>
-
-          <button className="btn" type="submit">Login</button>
-
-          <div className="links-login-modal-wrapper">
-            <p className="link-forgot-pass"  onClick={this.toggleEmailRecovery}>He olvidado la contraseña</p>
-            <p className="link-register">¿No tienes una cuenta? 
-              <span className="register-link" onClick={this.handleRegisterClick}>Regístrate</span>
-            </p>
-          </div>
-        </form>
+      <div>
+        <LoginFormFields
+          handleSubmit={this.handleSubmit}
+          handleChange={this.handleChange}
+          toggleEmailRecovery={this.toggleEmailRecovery}
+          handleRegisterClick={this.handleRegisterClick}
+          state={this.state}
+        />
       </div>
     )
   }
