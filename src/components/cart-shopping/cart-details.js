7b3a4eb74fa3ec2567e8b9e37609f6c5a27c64a0
Marimar <malonso@quadram.mobi>
2024-10-11 20:19:52 +0200

    modularizar en cart-shopping

--- src/components/cart-shopping/cart-details.js
+++ src/components/cart-shopping/cart-details.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import CartDetailsFormFields from '../forms/cart-details-form-fields';
 
 const CartDetails = ({ cartCourses = [], removeFromCart, openRegisterModal }) => {
     const history = useHistory();
@@ -12,14 +13,14 @@ const CartDetails = ({ cartCourses = [], removeFromCart, openRegisterModal }) =>
 
     const getDescounted = () => {
         return cartCourses
-        .reduce((sum, course) => {
-            const price = course.courses_discounted_price 
-                ? parseFloat(course.courses_discounted_price) 
-                : parseFloat(course.courses_price);
+            .reduce((sum, course) => {
+                const price = course.courses_discounted_price
+                    ? parseFloat(course.courses_discounted_price)
+                    : parseFloat(course.courses_price);
 
-            return sum + (isNaN(price) ? 0 : price);
-        }, 0)
-        .toFixed(2);
+                return sum + (isNaN(price) ? 0 : price);
+            }, 0)
+            .toFixed(2);
     };
 
     const handleDeleteClick = (courseId) => {
@@ -34,82 +35,22 @@ const CartDetails = ({ cartCourses = [], removeFromCart, openRegisterModal }) =>
         const token = localStorage.getItem("token");
         if (!token) {
             openRegisterModal();
-            if(token){
+            if (token) {
                 history.push('/cart-pay');
             }
-        } else { 
-            history.push('/cart-pay'); 
+        } else {
+            history.push('/cart-pay');
         }
     }
-   
-   
+
     return (
         <div className='content-page-wrapper'>
-            <div className='cart-paying-content-wrapper'>
-                <div className='cart-paying-header'>
-                    <h2>Cesta</h2>
-                </div>
-                <div className='cart-paying-details-wrapper'>
-                    <div className='cart-paying-products'>
-                        <div className='cart-paying-products-title'>
-                            <p>{cartCourses.length} cursos en total</p>
-                        </div>
-                        {cartCourses.length === 0 ? (
-                            <p>No items in the cart</p>
-                        ) : (
-                            cartCourses.map((course) => {
-                                const discountedPrice = parseFloat(course.courses_discounted_price);
-                                const price = Number(course.courses_price).toFixed(2);
-                                return (
-                                    <div key={course.courses_id} className='cart-paying-product-resumen'>
-                                        <div className='cart-paying-image'>
-                                            <img src={course.courses_image} alt={course.courses_title} />
-                                        </div>
-                                        <div className='cart-paying-title'>
-                                            <span className='course-title'>{course.courses_title}</span>
-                                            <span className='course-professor'>Nombre profesor</span>
-                                        </div>
-                                        {discountedPrice ? (
-                                            <div>
-                                        <div className='cart-paying-discounted-price'>
-                                            {discountedPrice} €
-                                            
-                                        </div>
-                                        <div className='cart-paying-price-through'>
-                                            {price} €
-                                        </div>
-                                        </div>
-                                        ) : (
-                                        <div className='cart-paying-price'>
-                                            {price} €
-                                        </div>
-                                        )}
-                                        <div className='cart-paying-delete'>
-                                            <span
-                                                className='cart-paying-delete-text'
-                                                onClick={() => handleDeleteClick(course.courses_id)}
-                                            >
-                                                Eliminar
-                                            </span>
-                                        </div>
-                                    </div>
-                                );
-                            })
-                        )}
-                    </div>
-                    <div className='cart-paying-total-pay'>
-                        <div className='cart-paying-total-pay-header'>
-                            Total:
-                        </div>
-                        <div className='cart-paying-total-pay-total'>
-                            {getDescounted()} €
-                        </div>
-                        <div className='cart-paying-total-pay-button'>
-                            <button className='btn-save' onClick={handleToPay}>Pagar</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <CartDetailsFormFields
+                cartCourses={cartCourses}
+                handleDeleteClick={handleDeleteClick}
+                handleToPay={handleToPay}
+                getDescounted={getDescounted}
+            />
         </div>
     );
 }
--- src/components/cart-shopping/cart-shopping.js
+++ src/components/cart-shopping/cart-shopping.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useHistory } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import CartShoppingFormFields from "../forms/cart-shopping-form-fields";
 
 const CartShopping = ({ isOpen, cartCourses = [] , removeFromCart}) => {
   const history = useHistory();
@@ -32,45 +32,14 @@ const CartShopping = ({ isOpen, cartCourses = [] , removeFromCart}) => {
   };
 
   return (
-   
-    <div className="cart-shopping-container">
-      <div className="cart-items">
-        {cartCourses.length === 0 ? (
-          <p>No items in the cart</p>
-        ) : (
-          cartCourses.map((course) => {
-            const discountedPrice = parseFloat(course.courses_discounted_price);
-            const price = Number(course.courses_price);
-            return (
-              <div key={course.courses_id} className="cart-item">
-                <div className="cart-item-image" >
-                  <img src={course.courses_image} alt={course.courses_title} />
-                </div>
-                <div className="cart-item-info">
-                  <p className="cart-item-title">{course.courses_title}</p>
-                  {discountedPrice ? (
-                    <p className="cart-item-price">{discountedPrice.toFixed(2)} €</p>
-                  ) : (
-                    <p className="cart-item-price">{price.toFixed(2)} €</p>
-                  )}
-                </div>
-                <div className='cart-item-icons'>
-                  <a className="icon-trash" onClick={() => handleDeleteClick(course.courses_id)}>
-                    <FontAwesomeIcon icon="trash" />
-                  </a>
-                </div>
-              </div>
-            );
-          })
-        )}
-      </div>
-      {cartCourses.length > 0 && (
-        <div className="cart-total">
-          <p>Total: <span>{getTotal()} €</span></p>
-        </div>
-      )}
-      <button className="cart-checkout-button" onClick={handleGoToCart}>Ir a la cesta</button>
-    </div>
+ 
+      <CartShoppingFormFields
+        handleDeleteClick={handleDeleteClick}
+        handleGoToCart ={handleGoToCart}
+        cartCourses = {cartCourses}
+        getTotal={getTotal}
+        isOpen= {isOpen}
+      />
   );
 };
 
